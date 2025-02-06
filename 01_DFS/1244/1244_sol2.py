03ef0eb15f2fa2b46d93a77ccc3bbf617a3e0888
유예은 <yeeunyoo99@naver.com>
2025-02-07 07:17:46 +0900

    homework for dfs_practice

--- 01_DFS/1244/1244_sol2.py
+++ 01_DFS/1244/1244_sol2.py
@@ -8,14 +8,19 @@ for test_case in range(1, tc+1):
     num_list = list(str(num))
     # print(num_list)
     len_num = list(str(num))
+    temp = [str(num)]
     # 인덱스를 담을 리스트 ans
     ans = [i for i in range(len(len_num))]
     #위치 조합을 구하기 _ 인덱스 기준으로 (따라서 permutation의 대상은 ans이다
     # 위치조합_case 리스트에 받기
-    case = list(itertools.permutations(ans,2))
-    # 위치조합을 중복순열로 cnt개씩 선택한 경우를 구하기 _ test_case
-    if cnt == 1:
-        test_case = case
-    else:
-        test_case = list(itertools.product(case,repeat = cnt))
-    print(test_case)
+    case = list(itertools.permutations(ans, 2))
+    for _ in range(cnt):
+        temp_2 = []
+        for i,j in case:
+            for e in temp:
+                list_e = list(e)
+                list_e[i], list_e[j] = list_e[j], list_e[i]
+                e = ''.join(list_e)
+                temp_2.append(e)
+        temp = list(set(temp_2))
+    print(f"#{test_case} {max(temp)}")
