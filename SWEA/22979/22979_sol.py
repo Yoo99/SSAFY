07d24caf51888c7ae84329154f427fde1b9e34fb
유예은 <yeeunyoo99@naver.com>
2025-02-24 17:58:51 +0900

    commit

--- SWEA/22979/22979_sol.py
+++ SWEA/22979/22979_sol.py
@@ -7,13 +7,16 @@ for test_case in range(1, T+1):
     K = int(input())
     cal = list(map(int, input().split()))
     for ele in cal:
-        if ele>0:
+        if ele == 0:continue
+        elif ele <0:
+            if abs(ele)>len(arr):
+                ele = abs(ele)%len(arr) * (-1)
+            e = arr[ele:]
+            arr = e + arr[:ele]
+        else:
+            if ele>len(arr):
+                ele = ele%len(arr)
             e = arr[:ele]
-            arr = arr[ele:] +e
-        elif ele<0:
-            for i in range(abs(ele)):
-                d = arr[-1]
-                arr = [d] + arr[:-1]
-        elif ele ==0:
-            continue
+            arr = arr[ele:] + e
+
     print(''.join(arr))
