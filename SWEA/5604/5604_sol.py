c49dc9a38f1fb90fce0a896a44c6a69a159381fa
유예은 <yeeunyoo99@naver.com>
2025-03-07 19:55:10 +0900

    commit

--- SWEA/5604/5604_sol.py
+++ SWEA/5604/5604_sol.py
@@ -1,1 +1,12 @@
+import sys
+sys.stdin = open("5604_input.txt")
 
+T = int(input())
+for test_case in range(1, T+1):
+    A, B = map(int, input().split())
+    cnt = 0
+    for i in range(A, B+1):
+        i  =str(i)
+        d= list(map(int, i))
+        cnt +=sum(d)
+    print(f"#{test_case} {cnt}")
