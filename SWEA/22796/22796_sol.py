35873954f748f90a1e6881116cc1d896e377364b
Yoo <yeeunyoo99@naver.com>
2025-03-07 17:51:46 +0900

    0307 final commit

---
+++ SWEA/22796/22796_sol.py
@@ -0,0 +1,6 @@
+import sys
+sys.stdin = open("22796_input.txt")
+
+T = int(input())
+for test_case in range(1, T+1):
+    arr= list(input())
---
+++ SWEA/23005/23005_sol.py
@@ -0,0 +1,5 @@
+import sys
+sys.stdin = open("23005_input.txt")
+
+T = int(input())
+for test_case in range(1, T+1):
